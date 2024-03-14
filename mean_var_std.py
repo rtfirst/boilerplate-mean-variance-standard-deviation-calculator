fc9551b951c327063624b56e07e5b928f29d1bd7
Roland Tfirst <roland@tfirst.de>
2024-03-14 08:06:01 +0000

    initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,23 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    matrix = np.reshape(list, (3, 3))
+
+    calculations = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.flatten().mean()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.flatten().var()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.flatten().std()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.flatten().max()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.flatten().min()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
 
 
+    for key, value in calculations.items():
+        for i in range(len(value)):
+            value[i] = value[i].tolist() if isinstance(value[i], np.ndarray) else value[i]
 
     return calculations
